1541a19b26c9b84ebe88d790bd785f41950ea2a8
Marcel Sousa <82828508+Tehses@users.noreply.github.com>
2022-08-16 15:27:41 +0100

    Initial Commit

---
+++ HackN3/HackN3App.swift
@@ -0,0 +1,17 @@
+//
+//  HackN3App.swift
+//  HackN3
+//
+//  Created by Marcel Sousa on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct HackN3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
